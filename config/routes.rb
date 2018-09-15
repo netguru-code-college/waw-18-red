28c5356ac0494dcd1b7823d671d417d945a404ac
Mirek <mirek@szul.eu>
2018-09-15 15:07:56 +0200

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get 'reviews/index'
-  get 'reviews/show'
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'places/index'
-  get 'places/show'
-  get 'places/new'
-  get 'places/create'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+
+  resources :reviews
+  resources :places
 end
