25c8f54e5ed0ef129ac9c051c189782f9c1a13e6
Surrerstry <surrerstry@gmail.com>
2018-09-15 14:41:53 +0200

    drop users table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_121559) do
+ActiveRecord::Schema.define(version: 2018_09_15_123729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,14 +31,6 @@ ActiveRecord::Schema.define(version: 2018_09_15_121559) do
     t.string "commenter"
     t.text "comment"
     t.integer "rate"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
