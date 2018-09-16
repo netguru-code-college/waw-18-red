ad39ec1c9cc583d034f1441f3c3edcee78aec843
voycik <voycik@gmail.com>
2018-09-16 13:52:46 +0200

    Renamed column commenter to user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_16_072243) do
+ActiveRecord::Schema.define(version: 2018_09_16_114313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,11 +28,12 @@ ActiveRecord::Schema.define(version: 2018_09_16_072243) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "commenter"
     t.text "comment"
     t.integer "rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
