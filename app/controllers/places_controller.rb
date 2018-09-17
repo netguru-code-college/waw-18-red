7b4911b2253e6febbf38831c42aa8da49bbe9f10
Mirek <mirek@szul.eu>
2018-09-17 17:27:27 +0200

    Add authentication of users to pages with forms

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,6 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @places = Place.all.paginate(page: params[:page], per_page: 5).order('updated_at DESC')
   end
