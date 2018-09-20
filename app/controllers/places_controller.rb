7c1c7032197db095db6e94162b6a64a9d19fcdf8
Mirek <mirek@szul.eu>
2018-09-21 00:06:44 +0200

    Add places counter to navbar

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :find_place, only: [:show, :edit, :update, :destroy, :find_place]
+  before_action :find_place, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :place_owner, only: [:edit, :update, :destroy]
 
